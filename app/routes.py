77f12a69469eb6e108c99d927fbc82a0ab6e5981
Tsunderarislime <terrarislime@gmail.com>
2026-01-23 23:13:03 -0800

    Added login framework, edited styling some more

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f'User: {form.username.data} logged in. Remember Me: {form.remember_me.data}.')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
